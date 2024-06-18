6d485884dc9396baeeca3255b264cc801635e6ef
Hyun Lee <hyunlee@Hyuns-MacBook-Pro.local>
2024-06-18 17:35:40 +0900

    [Feat] #9 Add Camera Authorization Checker

--- BalYeonGiAwards/ViewModels/CameraViewModel.swift
+++ BalYeonGiAwards/ViewModels/CameraViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import SwiftUI
+import AVFoundation
+
+class CameraViewModel{
+    enum Status{
+        case permitted
+        case notPermitted
+    }
+    
+    func checkCaptureAuthorizationStatus() async -> Status{
+        switch AVCaptureDevice.authorizationStatus(for: .video){
+        case .notDetermined:
+            let isPermissionGranted = await AVCaptureDevice.requestAccess(for: .video)
+            if isPermissionGranted {
+                return .permitted
+            }else{
+                fallthrough
+            }
+        case .authorized:
+            return .permitted
+        default:
+            return .notPermitted
+        }
+    }
+}
