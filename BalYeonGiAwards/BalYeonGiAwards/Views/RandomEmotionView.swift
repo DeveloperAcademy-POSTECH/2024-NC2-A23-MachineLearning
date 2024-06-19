3999698e29ac5fdb5db22b112d2a3dfb007a2618
Hyun Lee <hyunlee@Hyuns-MacBook-Pro.local>
2024-06-19 15:59:30 +0900

    [Feat] #18 Add Router pattern and timer to make screen go over in 3 seconds

--- BalYeonGiAwards/BalYeonGiAwards/Views/RandomEmotionView.swift
+++ BalYeonGiAwards/BalYeonGiAwards/Views/RandomEmotionView.swift
@@ -8,23 +8,39 @@
 import SwiftUI
 
 struct RandomEmotionView: View {
-    @State var num = 0
+    @ObservedObject var router = Router()
+    
     var body: some View {
-        ZStack{
-            Color.appBackground.ignoresSafeArea()
-            VStack{
-                Image("Spotlight")
-                Spacer()
-            }.ignoresSafeArea()
-            VStack{
-                ChooseEmotionAnimationView(name: "animation.json")
-                Text("어떤 감정으로\n연기대결을\n펼치게 될까요?")
-                    .font(.custom("HSSanTokki2.0-Regular", size: 40))
-                    .foregroundStyle(.white)
-                    .padding(.bottom, 106)
+        NavigationStack(path: $router.path){
+            ZStack{
+                Color.appBackground.ignoresSafeArea()
+                VStack{
+                    Image("Spotlight")
+                    Spacer()
+                }.ignoresSafeArea()
+                VStack{
+                    ChooseEmotionAnimationView(name: "animation.json")
+                    Text("어떤 감정으로\n연기대결을\n펼치게 될까요?")
+                        .font(.custom("HSSanTokki2.0-Regular", size: 40))
+                        .foregroundStyle(.white)
+                        .padding(.bottom, 106)
+                    Button{
+                        router.push(.EmotionResultView)
+                    }label:{
+                        Text("here")
+                    }
+                }
+            }
+            .navigationDestination(for: Router.Destination.self){destination in
+                router.view(for: destination)
+            }
+            .onAppear() {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5.0){
+                    print("hello")
+                    router.push(.EmotionResultView)
+                }
             }
         }
-        
     }
 }
 
