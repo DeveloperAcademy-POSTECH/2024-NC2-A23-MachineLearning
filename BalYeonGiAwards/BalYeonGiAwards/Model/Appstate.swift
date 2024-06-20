8192db92ba81fae8a4fce60d0bb75fdabc1a42f0
jiae0119 <ca090434@naver.com>
2024-06-20 19:54:02 +0900

    [Design, Feat] StartView, NumberOfPeopleView 구현

---
+++ BalYeonGiAwards/BalYeonGiAwards/Model/Appstate.swift
@@ -0,0 +1,18 @@
+//
+//  Appstate.swift
+//  BalYeonGiAwards
+//
+//  Created by JiaeShin on 6/20/24.
+//
+
+import SwiftUI
+
+struct Appstate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Appstate()
+}
---
+++ BalYeonGiAwards/BalYeonGiAwards/Views/SubViews/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  BalYeonGiAwards
+//
+//  Created by JiaeShin on 6/20/24.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
