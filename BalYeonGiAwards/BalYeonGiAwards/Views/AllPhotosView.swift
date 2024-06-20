5862f80e58d4cd1fe893fb7fb22ac5323c90cb9b
Hyun Lee <hyunlee@Hyuns-MacBook-Pro.local>
2024-06-20 10:13:59 +0900

    [Feat] Create AllPhotosView, ensure that UIUX is as intended, check that photos turn up in a correct format

--- BalYeonGiAwards/BalYeonGiAwards/Views/AllPhotosView.swift
+++ BalYeonGiAwards/BalYeonGiAwards/Views/AllPhotosView.swift
@@ -9,11 +9,50 @@ import SwiftUI
 
 struct AllPhotosView: View {
     @EnvironmentObject var cameraVM: CameraViewModel
+    private let adaptiveColumn = [
+        GridItem(.flexible(), spacing: 0),
+        GridItem(.flexible(), spacing: 0)
+    ]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .onAppear(){
-                print("\(cameraVM.photos)")
+        ZStack{
+            Color.appBackground.ignoresSafeArea()
+            VStack{
+                Image("Spotlight")
+                Spacer()
+            }.ignoresSafeArea()
+            VStack{
+                Text("멋진 대결이었어요!")
+                    .font(.custom("HSSanTokki2.0-Regular", size: 38))
+                    .foregroundStyle(.white)
+                    .padding(.top, 20)
+                Spacer()
+                LazyVGrid(columns: adaptiveColumn, spacing: 0) {
+                    ForEach(cameraVM.photos, id: \.self) { item in
+                        Image(uiImage: item)
+                            .resizable()
+                            .frame(width: 126, height: 126, alignment: .center)
+                    }
+                }
+                .frame(maxWidth: 252)
+                Spacer()
+                Button{
+                    print("Next")
+                }label:{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 14)
+                            .foregroundColor(.brown)
+                            .frame(width:353, height: 56)
+                        Text("심사 시작하기")
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                    }
+                }
+                .padding(.bottom, 72)
             }
+        }
+        .navigationBarBackButtonHidden()
     }
 }
 
