3458bc448b58481b0cd445a4b7b87b406b22297c
jiae0119 <ca090434@naver.com>
2024-06-20 19:56:02 +0900

    [Feat, Design] NumberOfPeopleView, StartView 구현

--- BalYeonGiAwards/BalYeonGiAwards/Model/Appstate.swift
+++ BalYeonGiAwards/BalYeonGiAwards/Model/Appstate.swift
@@ -7,12 +7,6 @@
 
 import SwiftUI
 
-struct Appstate: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    Appstate()
+class AppState: ObservableObject {
+    @Published var numberOfPeople: Int = 2
 }
--- BalYeonGiAwards/BalYeonGiAwards/Views/SubViews/StartView.swift
+++ BalYeonGiAwards/BalYeonGiAwards/Views/SubViews/StartView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct StartView: View {
+    @EnvironmentObject var router: Router
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+
+            Image("StartImage")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity)
+                .padding(.bottom, -30)
+            
+            Text("발연기\n시상식")
+                .font(.custom("HSSanTokki2.0-Regular", size: 64))
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity)
+                .padding(.top,0)
+
+            
+            Spacer()
+        
+            Button{
+                router.push(.NumberOfPeopleView)
+            }label:{
+                ZStack{
+                    RoundedRectangle(cornerRadius: 14)
+                        .foregroundColor(.brown)
+                        .frame(width:353, height: 56)
+                    Text("시상식 입장하기")
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                }
+            }
+            .padding(.bottom, 24)
+
+            
+        }
+        .background(Color.black)
+        .ignoresSafeArea(edges: .top)
     }
 }
 
